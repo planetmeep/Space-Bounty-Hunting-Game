e62a82f30478511046419d07553f6351e65aaaf6
Space-Dog <jasonbunce2@gmail.com>
2023-10-08 14:40:45 -0700

    Hooked up the input controls to delegates on the Ship Controller

--- Space Bounty Hunting Game/Assets/Scripts/ControlsManager.cs
+++ Space Bounty Hunting Game/Assets/Scripts/ControlsManager.cs
@@ -2,11 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
+//Different modes for Flying and Walking that assign delegates to the controls.
+//Added unset so you always start unset and can pass in the mode you want at start.
+public enum PlayerMode
+{
+     Unset,
+    Flying,
+    Walking
+}
 
 public class ControlsManager : MonoBehaviour
 {
+    //Mode for switching between flying and walking
+    public PlayerMode currentMode = PlayerMode.Unset;
     // Reference to the input actions
     private PlayerControls controls;
+    [SerializeField]
+    private ShipController shipController;
+
+    //Action Delegate
+    public Action onMoveForward;
+    public Action onTurnLeft;
+    public Action onTurnRight;
+    public Action onBackwards;
 
     private void Awake()
     {
@@ -16,30 +35,68 @@ public class ControlsManager : MonoBehaviour
         // Link the input actions to methods
         controls.Gameplay.MoveForward.performed += ctx => MoveForward();
         controls.Gameplay.TurnLeft.performed += ctx => TurnLeft();
-        controls.Gameplay.Backwards.performed += ctx => TurnBackwards();
+        controls.Gameplay.Backwards.performed += ctx => Backwards();
         controls.Gameplay.TurnRight.performed += ctx => TurnRight();
+
+        SetMode(PlayerMode.Flying);  // Ensure actions are hooked up at start
+    }
+    public void SetMode(PlayerMode mode)
+    {
+        Debug.Log("ShipController is: " + (shipController == null ? "NULL" : "Not NULL"));
+        Debug.Log("Mode is: " + " current mode is: " + currentMode);
+
+        if (currentMode == mode)
+        {
+             Debug.Log("Exiting early since mode is the same.");
+            return;
+        }
+
+        // Unsubscribe from all actions
+        onMoveForward -= shipController.MoveForward;
+        onTurnLeft -= shipController.TurnLeft;
+        onTurnRight -= shipController.TurnRight;
+        onBackwards -= shipController.Backwards;
+        
+
+        currentMode = mode;
+
+        switch (mode)
+        {
+            case PlayerMode.Flying:
+                onMoveForward += shipController.MoveForward;
+                onTurnLeft += shipController.TurnLeft;
+                onTurnRight += shipController.TurnRight;
+                onBackwards += shipController.Backwards;
+                Debug.Log("Subscribed to ShipController methods");
+                break;
+
+            case PlayerMode.Walking:
+                
+                break;
+        }
     }
 
     // Methods called when each action is performed
 
     private void MoveForward()
     {
-        Debug.Log("Moving forward.");
+        Debug.Log("ControlsManager: MoveForward Detected");
+        onMoveForward?.Invoke();
     }
 
     private void TurnLeft()
     {
-        Debug.Log("Turning left.");
+        onTurnLeft?.Invoke();
     }
 
-    private void TurnBackwards()
+    private void Backwards()
     {
-        Debug.Log("Moving backwards.");
+        onBackwards?.Invoke();
     }
 
     private void TurnRight()
     {
-        Debug.Log("Turning right.");
+        onTurnRight?.Invoke();
     }
 
     private void OnEnable()
---
+++ Space Bounty Hunting Game/Assets/Scripts/ShipController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public void MoveForward()
+    {
+        Debug.Log("Ship Moving Forward");
+
+    }
+
+    public void TurnLeft()
+    {
+        Debug.Log("Ship Turning Left");
+
+    }
+    public void TurnRight()
+    {
+        Debug.Log("Ship Turning Right");
+
+    }
+    public void Backwards()
+    {
+        Debug.Log("Backing that ass up");
+
+    }
+
+    
+}
