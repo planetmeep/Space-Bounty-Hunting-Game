69970587f8cc0c0394ec91a060a2c4a8b9905bdc
planetmeep <eliinfantado@gmail.com>
2023-11-20 22:25:44 -0500

    Death animation tweaks

--- Space Bounty Hunting Game/Assets/Scripts/KillableGroundNPC.cs
+++ Space Bounty Hunting Game/Assets/Scripts/KillableGroundNPC.cs
@@ -6,9 +6,15 @@ public class KillableGroundNPC : MonoBehaviour, IHittable
 {
     public Animator animator;
     public float health;
+    public float deathPushForce;
     public HitsoundMaterials hitsoundMaterial;
     public GameObject hitParticle;
+    public Rigidbody2D rb;
 
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     public void OnHit(Projectile projectile, Vector2 hitPoint, Quaternion hitDirection)
     {
         AudioManager.instance.PlayImpactSound(hitsoundMaterial);
@@ -19,6 +25,8 @@ public class KillableGroundNPC : MonoBehaviour, IHittable
             // Handle the death of the enemy
             animator.Play("Death");
             Instantiate(hitParticle, hitPoint, hitDirection);
+            Vector2 projectileToNPC = transform.position - (Vector3)hitPoint;
+            rb.AddForce(projectileToNPC * deathPushForce);
         }
     }
 }
